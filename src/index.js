a103cc3d36f86820e8e15ede1235b6c93267daf0
AV-Shell <avshell.sj@gmail.com>
2020-03-15 17:39:21 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!(Array.isArray(array))){
+    return 0;
+  }
+  if (array.length === 0){
+    return 0;
+  }
+  let minimal = array[0];
+  array.map( (s) => {minimal = s<minimal ?s:minimal;  })
+  return minimal;
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!(Array.isArray(array))){
+    return 0;
+  }
+  if (array.length === 0){
+    return 0;
+  }
+  let maximal = array[0];
+  array.map( (s) => {maximal = s>maximal ?s:maximal;  })
+  return maximal;
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!(Array.isArray(array))){
+    return 0;
+  }
+  if (array.length === 0){
+    return 0;
+  }
+  let summ = 0;
+  array.map( (s) => {summ+=s;  })
+  return summ/array.length;
 }
